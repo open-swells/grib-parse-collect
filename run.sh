2189c175d801c411a939e59967329593c9977d56
evancoons22 <evancoons22@gmail.com>
2025-10-15 19:42:19 -0700

    run.sh update

--- run.sh
+++ run.sh
@@ -3,13 +3,14 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+echo "Deleting local grib files"
+"$SCRIPT_DIR/scripts/delete_gribs.sh"
+
 echo "Starting grib parse runner"
 "$SCRIPT_DIR/scripts/grib_parse_runner.sh"
 
 echo "Copying files to server"
 "$SCRIPT_DIR/scripts/grib_copy_to_server.sh"
 
-echo "Deleting local grib files"
-"$SCRIPT_DIR/scripts/delete_gribs.sh"
 
 echo "All scripts executed successfully"
